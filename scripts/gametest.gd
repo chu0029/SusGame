c9ae14425587f8f973c267998ebb2c0b7259c833
sharkyskib <pra0018@suzannecoryhs.vic.edu.au>
2025-06-24 10:21:49 +1000

    added multiplier

--- scripts/gametest.gd
+++ scripts/gametest.gd
@@ -2,10 +2,11 @@ extends Control
 
 @export var counter: RichTextLabel
 @export var multipcounter: Label
+@export var timer: Timer
 
 var power: int = 0
-
-var multiplier : int = 0
+var multiplier : int = 1
+var automult : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,8 +19,21 @@ func UpdateLabelText() -> void:
 	counter.text = "Power: %s" %power
 	multipcounter.text = "Multiplier: %s" %multiplier
 
-func _on_factory_pressed() -> void:
+func CreatePower() -> void:
 	power += 1 * multiplier
+
+func AutoFactory() -> void: 
+	power += 1 * automult
+
+func _on_factory_pressed() -> void:
+	CreatePower()
 
 func _on_multiplier_pressed() -> void:
 	multiplier += 1
+
+func _on_autoclicker_pressed() -> void:
+	timer.start()
+	automult += 1
+
+func _on_timer_timeout() -> void:
+	AutoFactory()
