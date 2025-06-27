92cefc66787dcc5fcfa1dc88d7f6a9da415c833d
chu0029 <chu0029@suzannecoryhs.vic.edu.au>
2025-06-27 11:07:51 +1000

    cost feature added

--- scripts/gametest.gd
+++ scripts/gametest.gd
@@ -5,10 +5,13 @@ extends Control
 @export var timer: Timer
 @export var soundfactory: AudioStreamPlayer
 @export var soundupgrade: AudioStreamPlayer
+@export var autoclicker: Button
 
 var power: int = 0
 var multiplier : int = 1
 var automult : int = 0
+
+var AutomultC = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -36,9 +39,12 @@ func _on_multiplier_pressed() -> void:
 	soundupgrade.play()
 
 func _on_autoclicker_pressed() -> void:
-	timer.start()
-	automult += 1
-	soundupgrade.play()
+	if power >= AutomultC:
+		timer.start()
+		automult += 1
+		power -= 10
+		autoclicker.text = 'Autoclicker \n %c' %power
+		soundupgrade.play()
 
 func _on_timer_timeout() -> void:
 	AutoFactory()
