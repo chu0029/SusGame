ca7a2ad124c6ef6e38ac531554cfe776b74d817e
sharkyskib <pra0018@suzannecoryhs.vic.edu.au>
2025-06-27 12:00:32 +1000

    added solar panel

--- scripts/gametest.gd
+++ scripts/gametest.gd
@@ -6,12 +6,15 @@ extends Control
 @export var soundfactory: AudioStreamPlayer
 @export var soundupgrade: AudioStreamPlayer
 @export var autoclicker: Button
+@export var solarpanel: Button
 
 var power: int = 0
 var multiplier : int = 1
 var automult : int = 0
 
 var AutoclickC = 10
+
+var solarpanelcount : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,7 +24,7 @@ func _process(delta: float) -> void:
 	UpdateLabelText()
 	
 func UpdateLabelText() -> void:
-	counter.text = "Power: %s kWh" %power
+	counter.text = "Power: %s kW" %power
 	multipcounter.text = "Multiplier: %s" %multiplier
 
 func CreatePower() -> void:
@@ -29,6 +32,7 @@ func CreatePower() -> void:
 
 func AutoFactory() -> void: 
 	power += 1 * automult
+	power += 1 * solarpanelcount
 
 func _on_factory_pressed() -> void:
 	CreatePower()
@@ -49,3 +53,14 @@ func _on_autoclicker_pressed() -> void:
 
 func _on_timer_timeout() -> void:
 	AutoFactory()
+
+
+func _on_solarpanel_pressed() -> void:
+	if power >= 10:
+		timer.start()
+		solarpanelcount += 1
+		power -= 10
+		solarpanel.text = 'Solar Panel: %s \n Cost: 10 kW \n 1 kW/s' %solarpanelcount
+		soundupgrade.play()
+		
+		
