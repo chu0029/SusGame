844fe7003c8e8eaad8602a1b416d61901f090f95
chu0029 <chu0029@suzannecoryhs.vic.edu.au>
2025-07-02 19:55:36 +1000

    container stuff

--- scripts/gametest.gd
+++ scripts/gametest.gd
@@ -19,6 +19,7 @@ var multiplier : int = 1
 var automult : int = 0
 
 var AutoclickC = 10
+var solarC = 25
 
 var solarpanelcount : int = 0
 var windturbinecount : int = 0
@@ -28,7 +29,6 @@ var geothermalplantcount : int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	UpdateLabelText()
-	bgm.play()
 	
 func _process(delta: float) -> void:
 	UpdateLabelText()
@@ -53,7 +53,8 @@ func _on_factory_pressed() -> void:
 func _on_multiplier_pressed() -> void:
 	multiplier += 1
 	soundupgrade.play()
-
+	bgm.play()
+	
 func _on_autoclicker_pressed() -> void:
 	if power >= AutoclickC:
 		timer.start()
@@ -68,11 +69,12 @@ func _on_timer_timeout() -> void:
 
 
 func _on_solarpanel_pressed() -> void:
-	if power >= 25:
+	if power >= solarC:
 		timer.start()
 		solarpanelcount += 1
 		power -= 25
-		solarpanel.text = 'Solar Panel: %s \n Cost: 25 kW \n Produces 1 kW/s' %solarpanelcount
+		solarC = round(solarC*1.5)
+		solarpanel.text = 'Solar Panel: %s \n Cost: %t kW \n Produces 1 kW/s' %solarpanelcount %solarC
 		soundupgrade.play()
 
 func _on_windturbine_pressed() -> void:
