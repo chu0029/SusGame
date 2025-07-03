a616e4299d0f07ac29aa1f5398badfa408d6e323
Andehheh78 <he-0011@suzannecoryhs.vic.edu.au>
2025-07-04 09:26:05 +1000

    clean stuff up

--- scripts/menu_start_script.gd
+++ scripts/menu_start_script.gd
@@ -10,11 +10,5 @@ func _ready():
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/gametest.tscn")
 
-
-
-func _on_SETTINGS_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
